a2824b4b7e4523b90322a373affea3c4041c28dc
Md. Maruf Mullah <md.marufmullah50@gmail.com>
2026-02-11 12:13:04 +0600

    video tracking added

--- live_tracking(translate_sign)/main.py
+++ live_tracking(translate_sign)/main.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 from PIL import Image   
 import cv2
 
-model = YOLO("best.pt")
+model = YOLO("G:\\Github_Projects\\YOLO_Projects\\live_tracking(translate_sign)\\best.pt")
 results = model.predict(source="0", show=True)
